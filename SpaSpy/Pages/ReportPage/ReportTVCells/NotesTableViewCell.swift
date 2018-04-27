fec14e59a0a80f8c862caf3703b6fb71ddf174a2
Margaret Chan <margaret.helen.chan@gmail.com>
2018-04-27 12:31:40 -0400

    Create SignInPage on storyboard, Create custom table view cells for ReportPage

---
+++ SpaSpy/Pages/ReportPage/ReportTVCells/NotesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotesTableViewCell.swift
+//  SpaSpy
+//
+//  Created by C4Q on 4/27/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SpaSpy/Pages/ReportPage/ReportTVCells/PhotosTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotosTableViewCell.swift
+//  SpaSpy
+//
+//  Created by C4Q on 4/27/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
